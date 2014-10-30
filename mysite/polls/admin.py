cdf5c14a6f7a0f83f2749fb348b08039625b46a5
Daltin Loomis <loomisdf@gmail.com>
2014-10-29 22:36:46 -0400

    add list display to question admin in admin.py. Add list_filter to the corresponding class in models.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -14,6 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInLine]
 
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
