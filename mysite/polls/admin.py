ccf9e0793021d872e250ffc2b12f2c0390f14390
Daltin Loomis <loomisdf@gmail.com>
2014-10-29 22:29:04 -0400

    add list_display to the QuestionAdmin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -13,5 +13,7 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInLine]
 
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
