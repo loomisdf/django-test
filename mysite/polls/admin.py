c614bf5a7ccd617f6c84a1d2608634a588a48391
Daltin Loomis <loomisdf@gmail.com>
2014-10-29 21:53:20 -0400

    add ChoiceInLine class so that choices now appear on the question add page. This makes creating choices for question super easy

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 # Register your models here.
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
